973ee98af50828e08d59dd65d39baac3040e4c6d
Angel Manuel <amayhuag@uni.pe>
2022-06-24 13:22:20 -0500

    Initial State

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [{
+        id: 1,
+        img: 'https://res.cloudinary.com/dhklu5lqa/image/upload/v1653320763/e-commerce_app_react/pic1_osxuwb.jpg',
+        title: 'SUMMER SALE',
+        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: 'f5fafd',
+    },
+    {
+        id: 2,
+        img: 'https://res.cloudinary.com/dhklu5lqa/image/upload/v1653320764/e-commerce_app_react/pic2_zkphqa.jpg',
+        title: 'CHRISTMAS SALE',
+        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: 'fcf1ed',
+    },
+    {
+        id: 3,
+        img: 'https://res.cloudinary.com/dhklu5lqa/image/upload/v1653320767/e-commerce_app_react/pic3_pfpjaf.jpg',
+        title: 'AUTUMN SALE',
+        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: 'fbf0f4',
+    },
+];
+
+export const categories = [{
+        id: 1,
+        img: 'https://res.cloudinary.com/dhklu5lqa/image/upload/v1654790641/e-commerce_app_react/categories/women.jpg',
+        title: 'WOMEN',
+        cat: 'women',
+    },
+    {
+        id: 2,
+        img: 'https://res.cloudinary.com/dhklu5lqa/image/upload/v1654790641/e-commerce_app_react/categories/bottom.jpg',
+        title: 'TOPS',
+        cat: 'top',
+    },
+    {
+        id: 3,
+        img: 'https://res.cloudinary.com/dhklu5lqa/image/upload/v1654790641/e-commerce_app_react/categories/top.jpg',
+        title: 'BOTTOMS',
+        cat: 'bottom',
+    },
+];
+
+export const popularProducts = [{
+        id: 1,
+        img: 'https://res.cloudinary.com/dhklu5lqa/image/upload/v1653340625/e-commerce_app_react/clo1_hpy9yi.jpg  ',
+    },
+    {
+        id: 2,
+        img: 'https://res.cloudinary.com/dhklu5lqa/image/upload/v1653340625/e-commerce_app_react/clo2_ps6lo7.jpg',
+    },
+    {
+        id: 3,
+        img: 'https://res.cloudinary.com/dhklu5lqa/image/upload/v1653340629/e-commerce_app_react/clo3_e6sdh8.jpg',
+    },
+    {
+        id: 4,
+        img: 'https://res.cloudinary.com/dhklu5lqa/image/upload/v1653340626/e-commerce_app_react/clo4_dchpoc.jpg',
+    },
+    {
+        id: 5,
+        img: 'https://res.cloudinary.com/dhklu5lqa/image/upload/v1653340627/e-commerce_app_react/clo5_xej3co.jpg',
+    },
+    {
+        id: 6,
+        img: 'https://res.cloudinary.com/dhklu5lqa/image/upload/v1653340626/e-commerce_app_react/clo6_oxwqak.jpg',
+    },
+    {
+        id: 7,
+        img: 'https://res.cloudinary.com/dhklu5lqa/image/upload/v1653340630/e-commerce_app_react/clo7_uabijk.jpg',
+    },
+    {
+        id: 8,
+        img: 'https://res.cloudinary.com/dhklu5lqa/image/upload/v1653340628/e-commerce_app_react/clo8_wqezgb.jpg',
+    },
+];
